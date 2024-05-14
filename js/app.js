a36630c656be63d1d15a66491820b1b956036563
HuseynliTogrul <toghrulih@code.edu.az>
2024-05-15 01:56:20 +0400

    Project

--- js/app.js
+++ js/app.js
@@ -3,60 +3,64 @@ const CategoryHeader = document.querySelector(".CategoryHeader");
 
 const searchIcon = document.querySelector(".searchIcon");
 const searchSection = document.querySelector(".searchSection");
-
-const searchButton = document.querySelector(".search button");
+const closeSearch = document.querySelector("#close-search");
 
 const productIcon = document.querySelector(".productIcon i")
 
+const menubarIcon = document.querySelector(".menubarIcon i")
+const menuMobile = document.querySelector(".menu-mobile")
+const menuHeaderIcon = document.querySelector(".menu-header i")
+
+
 
 window.onkeydown = function (event) {
-    if (event.keyCode == 27) {        
-        searchSection.style.display = "none";
-    }
+  if (event.keyCode == 27) {
+    searchSection.style.display = "none";
+  }
 };
 
 CategoryHeader.addEventListener("click", () => {
-    categoryList.classList.toggle("DisplayNone");
+  categoryList.classList.toggle("DisplayNone");
 })
+
+// Search
 
 searchIcon.addEventListener("click", () => {
-    if (searchSection.style.display === 'block') {
-        searchSection.style.display = 'none';
-    } else {
-        searchSection.style.display = 'block';
-    }
+  searchSection.style.display = "block";
+  document.body.style.overflow = 'hidden';
 })
 
-searchButton.addEventListener("click", () => {
-    searchSection.style.display = "none";
+closeSearch.addEventListener('click', () => {
+  searchSection.style.display = 'none';
+  document.body.style.overflow = 'auto';
 })
 
+// MenuBar
 
+menubarIcon.addEventListener("click", () => {
+  if (menubarIcon.style.display === "none") {
+    menuMobile.style.display = "none";
+  } else {
+    menuMobile.style.display = "block";
+  }
+})
 
+menuHeaderIcon.addEventListener("click", () => {
+  menuMobile.style.display = "none";
+})
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
+// Slider
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName(".slide");
-//   let point = document.getElementsByClassName("slidePoint p");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < point.length; i++) {
-//     point[i].className = point[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   point[slideIndex-1].className += " active";
-// }
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
--- js/basket.js
+++ js/basket.js
@@ -18,3 +18,146 @@ minusCount.addEventListener("click", () => {
     }
     counter.value = count;
 })
+
+
+// ///////////////////////
+
+
+
+// prodListItem.forEach(items => {
+//     const input = items.querySelector('input[type="number"]');
+//     const plus = items.querySelector('.plus');
+//     const minus = items.querySelector('.minus');
+//     var itemTotal = items.querySelector('.item-total p');
+//     var DeleteBtn = items.querySelector('.item-delete');
+//     const sellPrice = items.querySelector('.sellPrice');
+//     const discPrice = items.querySelector('discPrice');
+
+
+//     let itemTotalfixed = itemTotal.innerText.replace('$', '').trim();
+//     let currentQuantity = parseFloat(input.value);
+//     let sellPricefixed = sellPrice.innerText.replace('$', '').trim();
+
+
+
+//     itemTotal.innerText = '$' + `${(input.value) * parseFloat(sellPricefixed)}`;
+//     if (discPrice != null) {
+//         let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//         totalPrice += discPricefixed * input.value
+//     } else {
+//         totalPrice += parseFloat(sellPricefixed) * input.value
+//     }
+//     subTotal.innerText = '$' + totalPrice;
+
+
+
+//     if (discPrice != null) {
+//         let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//         disAmount += (discPricefixed - parseFloat(sellPricefixed)) * input.value;
+
+//         discountAmount.innerText = '$' + disAmount;
+//     }
+//     cartTotal.innerText = '$' + (totalPrice - disAmount);
+
+
+
+//     totalItem += parseInt(input.value);
+//     cartTotalItem.innerText = totalItem;
+//     cartTotalHeadItem.innerText = totalItem;
+
+
+
+//     plus.addEventListener('click', () => {
+//         var value = parseFloat(input.value);
+//         input.value = value + 1;
+
+//         itemTotal.innerText = '$' + `${(value + 1) * parseFloat(sellPricefixed)}`;
+
+//         if (discPrice != null) {
+//             let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//             totalPrice += discPricefixed
+//         } else {
+//             totalPrice += parseFloat(sellPricefixed)
+//         }
+
+//         subTotal.innerText = '$' + totalPrice;
+
+//         if (discPrice != null) {
+//             let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//             disAmount += (discPricefixed - parseFloat(sellPricefixed));
+
+//             discountAmount.innerText = '$' + disAmount;
+//         }
+//         cartTotal.innerText = '$' + (totalPrice - disAmount);
+
+
+//         ++totalItem; 
+//         cartTotalItem.innerText = totalItem;
+//         cartTotalHeadItem.innerText = totalItem;
+
+//     });
+
+
+
+//     minus.addEventListener('click', () => {
+//         var value = parseFloat(input.value);
+//         if (value > 1) {
+//             input.value = value - 1;
+//             itemTotal.innerText = '$' + `${(value - 1) * parseFloat(sellPricefixed)}`;
+
+//             if (discPrice != null) {
+//                 let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//                 totalPrice -= discPricefixed
+//             } else {
+//                 totalPrice -= parseFloat(sellPricefixed)
+//             }
+
+//             subTotal.innerText = '$' + totalPrice;
+
+//             if (discPrice != null) {
+//                 let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//                 disAmount -= (discPricefixed - parseFloat(sellPricefixed));
+
+//                 discountAmount.innerText = '$' + disAmount;
+//             }
+
+//             cartTotal.innerText = '$' + (totalPrice - disAmount);
+
+
+//             --totalItem;
+//             cartTotalItem.innerText = totalItem;
+//             cartTotalHeadItem.innerText = totalItem;
+//         }
+
+//     });
+
+
+
+//     DeleteBtn.addEventListener('click', () => {
+//         if (discPrice != null) {
+//             let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//             totalPrice -= discPricefixed * input.value
+//         } else {
+//             totalPrice -= parseFloat(sellPricefixed) * input.value
+//         }
+
+//         subTotal.innerText = '$' + totalPrice;
+
+//         if (discPrice != null) {
+//             let discPricefixed = parseFloat(discPrice.innerText.replace('$', '').trim());
+//             disAmount -= (discPricefixed - parseFloat(sellPricefixed)) * input.value;
+
+//             discountAmount.innerText = '$' + disAmount;
+//         }
+
+//         cartTotal.innerText = '$' + (totalPrice - disAmount);
+
+//         totalItem -= input.value;
+//         cartTotalItem.innerText = totalItem;
+//         cartTotalHeadItem.innerText = totalItem;
+
+//         items.remove();
+//     });
+
+
+// });
